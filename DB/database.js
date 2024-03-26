331250c41cb1d1ceae492ac65f03901c455bc8ea
michalk1924 <michalk1924@gmail.com>
2024-03-26 21:09:09 +0200

    more

--- DB/database.js
+++ DB/database.js
@@ -2,31 +2,42 @@ var mysql = require('mysql2');
 require('dotenv').config();
 
 var con = mysql.createConnection({
-  host: process.env.HOST_NAME,
-  user: process.env.USER,
-  port: process.env.PORTSQL,
-  password: process.env.PASSWORD,
-  //database: process.env.DATABASE
+    host: process.env.HOST_NAME,
+    user: process.env.USER,
+    port: process.env.PORTSQL,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE
 });
 
-
-exports.createDB = () => {
+const createDB = () => {
     con.connect(function (err) {
-      if (err) throw err;
-      console.log("Connected!");
-      var sql = "CREATE DATABASE jsonplaceholderDB;";
-      con.query(sql, function (err, result) {
         if (err) throw err;
-        console.log("Table created");
-      });
+        console.log("Connected!");
+        var sql = "CREATE DATABASE jsonplaceholderDB;";
+        con.query(sql, function (err, result) {
+            if (err) throw err;
+            console.log("database created");
+        });
     });
-  }
+}
 
-exports.createTableCustomers = () => {
-  con.connect(function (err) {
-    if (err) throw err;
-    console.log("Connected!");
-    var sql = String.raw`CREATE TABLE customers (
+const deleteTable = (tableName) => {
+    con.connect(function (err) {
+        if (err) throw err;
+        console.log("Connected!");
+        var sql = `DROP TABLE ${tableName};`;
+        con.query(sql, function (err, result) {
+            if (err) throw err;
+            console.log("Table created");
+        });
+    });
+}
+
+const createTableCustomers = async () => {
+    con.connect(function (err) {
+        if (err) ;
+        console.log("Connected!");
+        var sql = String.raw`CREATE TABLE customers (
       firstName VARCHAR(255),
       lastName VARCHAR(255),
       id INT,
@@ -39,25 +50,27 @@ exports.createTableCustomers = () => {
       DateOfReceivingAPositiveResult DATE,
       dateOfRecovery DATE
   );`;
-    con.query(sql, function (err, result) {
-      if (err) throw err;
-      console.log("Table created");
+        con.query(sql, function (err, result) {
+            if (err) throw err;
+            console.log("Table created");
+        });
     });
-  });
 }
 
-exports.createTableVaccinations = () => {
-  con.connect(function (err) {
-    if (err) throw err;
-    console.log("Connected!");
-    var sql = String.raw`CREATE TABLE vaccinations (
+const createTableVaccinations = () => {
+    con.connect(function (err) {
+        if (err) throw err;
+        console.log("Connected!");
+        var sql = String.raw`CREATE TABLE vaccinations (
     customerId int,
     date DATE,
     manufacturer VARCHAR(255)
   );`;
-    con.query(sql, function (err, result) {
-      if (err) throw err;
-      console.log("Table created");
+        con.query(sql, function (err, result) {
+            if (err) throw err;
+            console.log("Table created");
+        });
     });
-  });
 }
+
+module.exports = { createDB, deleteTable };
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
-const cors = require('cors');
-//const database = require('./DB/database')
+//const cors = require('cors');
+const database = require('./DB/database')
 
 // const studentsRouter = require('./routes/students');
 // const teachersRouter = require('./routes/teachers');
@@ -11,21 +11,19 @@ const server = express();
 const host = process.env.HOST_NAME;
 const port = process.env.PORT_SERVER;
 
-server.use((req, res, next) => {
-    console.log('before')
-    next();
-})
-
-//database.createDB()
+// server.use((req, res, next) => {
+//     console.log('before')
+//     next();
+// })
 
 server.use(express.json());
 
-server.use(cors({
-    origin: 'http://localhost:5173', 
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Origin', 'X-Requested-With', 'Accept', 'x-client-key', 'x-client-token', 'x-client-secret', 'Authorization'],
-    credentials: true
-}))
+// server.use(cors({
+//     origin: 'http://localhost:5173', 
+//     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'],
+//     allowedHeaders: ['Content-Type', 'Origin', 'X-Requested-With', 'Accept', 'x-client-key', 'x-client-token', 'x-client-secret', 'Authorization'],
+//     credentials: true
+// }))
 
 // server.use('/students', studentsRouter)
 // server.use('/teachers', teachersRouter);
